2adce234d1cb4be2ef1157d78a1ac1c958998f0a
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-04-21 19:45:45 -0700

    9-print_comb.c committed

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'a program that prints all possible combinations
+ * of single-digit numbers.'
+ * Return: Always return 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 49; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
