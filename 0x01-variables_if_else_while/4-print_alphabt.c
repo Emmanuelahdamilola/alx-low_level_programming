08e19caf761b7c3ec39a54a01ed3b8413c8d531d
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-04-21 18:11:30 -0700

    4-print_alphabt.c committed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'a program that prints the alphabet in lowercase,
+ * followed by a new line.'
+ * Return: Always return 0
+*/
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		if (i == 101 || i == 113)
+		{
+			i++;
+			continue;
+		}
+		putchar(i);
+		i++;
+	}
+		putchar('\n');
+	return (0);
+}
