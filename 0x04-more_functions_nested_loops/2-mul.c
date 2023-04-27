4947eacba52422d40c2f14d121180aa987d09882
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-04-27 08:35:25 -0700

    2-mul.c file committed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a: input an integer
+ * @b: input an integer
+ * Return: return 0 success
+*/
+int mul(int a, int b)
+{
+	int n;
+
+	n = a * b;
+	return (n);
+}
