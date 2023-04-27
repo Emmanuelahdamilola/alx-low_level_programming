c8416e1c43a5ff27653e301f83386f159ba12a90
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-04-27 07:43:19 -0700

    0-isupper.c file committed

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - show 1 if the input is a
+ * uppercase character. the rest in cases, show 0
+ * @c: uses a character in ASCII code
+ * Return: return 1 for uppercase c, and 0 for the rest
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
