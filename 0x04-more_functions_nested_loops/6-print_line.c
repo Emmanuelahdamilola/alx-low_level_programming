4d3176dad5ecffee0b04340ed8ebd33c7012b72f
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-04-28 01:15:55 -0700

    4-print_most_numbers.c file committed

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: input an integer
+ * Return: retrun void
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (i == 95)
+	if (n > 0)
+	{
+		_putchar(i * n);
+	}
+	else
+	{
+		n <= 0;
+		_putchar('\n');
+	}
+}
