da225c660a6401c3f8bb7899eaa81f12acb48921
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-04 21:49:06 -0700

    7-leet.c file committed

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @n: integer
+ * Return: pointer to value
+ *
+*/
+char *leet(char *n)
+{
+	int m, x;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (m = 0; n[m] != '\0'; m++)
+	{
+		for (n = 0; x < 10; x++)
+		{
+			if (n[m] == s1[x])
+			{
+				n[m] = s2[x];
+			}
+		}
+	}
+	return (n);
+}
+
