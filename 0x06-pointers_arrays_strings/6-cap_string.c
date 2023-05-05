0bc345a1ac64adbbe85e2dfa606ad47744b6a6ce
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-04 21:36:43 -0700

    6-cap_string.c file committed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: return pointer
+*/
+char *cap_string(char *str)
+{
+	int ind = 0;
+
+	while (str[ind])
+	{
+		while (!(str[ind] >= 'a' && str[ind] <= 'z'))
+			ind++;
+
+		if (str[ind - 1] == ' ' ||
+		    str[ind - 1] == '\t' ||
+		    str[ind - 1] == '\n' ||
+		    str[ind - 1] == ',' ||
+		    str[ind - 1] == ';' ||
+		    str[ind - 1] == '.' ||
+		    str[ind - 1] == '!' ||
+		    str[ind - 1] == '?' ||
+		    str[ind - 1] == '"' ||
+		    str[ind - 1] == '(' ||
+		    str[ind - 1] == ')' ||
+		    str[ind - 1] == '{' ||
+		    str[ind - 1] == '}' ||
+		    ind == 0)
+			str[ind] -= 32;
+
+		ind++;
+	}
+
+	return (str);
+}
+
