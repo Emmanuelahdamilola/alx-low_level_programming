40a820fff91a88a885abab5465e5d9245d9edfe4
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-12 16:06:58 -0700

    my static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,27 +8,29 @@
 */
 int _atoi(char *s)
 {
-int a = 0;
-unsigned int pi = 0;
-int mi = 1;
-int si = 0;
+	int a = 0;
+	unsigned int pi = 0;
+	int mi = 1;
+	int si = 0;
 
-while (s[a])
-{
-if (s[a] == 45)
-{
-mi *= -1;
-}
-while (s[a] >= 48 && s[a] <= 57)
-{
-si = 1;
-pi = (pi * 10) + (s[a] - '0');
-a++;
-}
-if (si == 1)
-{
-break;
-}
-a++;
-}
+	while (s[a])
+	{
+		if (s[a] == 45)
+		{
+			mi *= -1;
+		}
+	while (s[a] >= 48 && s[a] <= 57)
+	{
+		si = 1;
+		pi = (pi * 10) + (s[a] - '0');
+		a++;
+	}
+	if (si == 1)
+	{
+		break;
+	}
+	a++;
+	}
+	pi *= mi;
+	return (pi);
 }
