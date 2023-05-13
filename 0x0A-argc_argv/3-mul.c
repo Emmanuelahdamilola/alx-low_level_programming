ce8caf9eaa8dc814f2170bc6c407342b1b722d77
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-12 17:31:56 -0700

    3-mul.c committed

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+	
+	mul = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		mul = mul * atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
