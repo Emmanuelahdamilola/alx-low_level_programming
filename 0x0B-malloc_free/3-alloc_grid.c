d62fb5f74cdd8c3a5993b890b44b0620ad621a46
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-16 07:13:06 -0700

    3-alloc_grid.c committed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		dim[a] = malloc(sizeof(int) * width);
 		if (dim[a] == NULL)
 		{
-			for (;a  >= 0; a--)
+			for (; a  >= 0; a--)
 				free(dim[a]);
 			free(dim);
 			return (NULL);
