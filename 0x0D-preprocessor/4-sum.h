11a2a385e9659810ba934aae337dfcc05f7aa7f1
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-22 14:06:43 -0700

    4-sum.h file committed

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+/**
+ * Description: function-like macro SUM(x, y) that
+ * computes the sum of the numbers x and y.
+ * @x: number to be added
+ * @y: number to be added
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
