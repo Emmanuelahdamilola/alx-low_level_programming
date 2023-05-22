f22d7a5d71ab046bf3e624da629e72fb082bba47
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-22 16:37:11 -0700

    dog.h file updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,18 +2,23 @@
 #define MACROS_H
 
 /**
- * dog_t - structure tag for the structure elements
+ *struct dog - structure tag for the structure elements
  * @name: structure member
  * @age: structure member
  * @owner: structure member
  */
 
-struct dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dot_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
