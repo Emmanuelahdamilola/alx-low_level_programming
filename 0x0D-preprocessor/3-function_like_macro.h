47be5831dc1046054367c9512d1cfa62ac60dbf0
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-22 13:59:10 -0700

    3-function_like_macro.h file committed

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+/**
+ * Description: functio-like macro ABS(X)that
+ * computes the absolute value of a number x
+ * @x: absolute value
+ */
+
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+#endif
