102721043908b2b3efd93591fd7b98c6bd788e56
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-22 15:32:52 -0700

    dog.h file committed

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+/**
+ * struct dog - structure tag for the structure elements
+ * @name: structure member
+ * @age: structure member
+ * @owner: structure member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
