9b88869ead27cf3a207f508850e869174b3e38c4
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-05-22 18:03:56 -0700

    4-new_dog.c file committed

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * free_dog - function that free dogs
+ * @d: free dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+
+	free(d);
+}
