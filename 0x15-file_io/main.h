2b657d8989ff6166a67c3eca89889bf56ea992ee
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-06-13 19:16:00 -0700

    File i/o header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -24,7 +24,7 @@ void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf)
+void close_elf(int elf);
 
 
 
