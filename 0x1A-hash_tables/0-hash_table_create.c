1857ee9812f855e2dd66381905317f725a311d22
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-08-11 11:19:01 -0700

    function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -26,9 +26,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
+		free(table->array);
 		free(table);
 		return (NULL);
 	}
 
 	return (table);
 }
+
